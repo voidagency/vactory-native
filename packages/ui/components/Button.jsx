22c2c19d892d4efcfb2e6a37c0a7cfcdb2809b5f
h.bahlaouane <h.bahlaouane@void.fr>
2023-02-16 11:20:05 +0100

    fix(ui): button change color

--- packages/ui/components/Button.jsx
+++ packages/ui/components/Button.jsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 32,
 		borderRadius: 4,
 		elevation: 3,
-		backgroundColor: "black",
+		backgroundColor: "tomato",
 	},
 	text: {
 		fontSize: 16,
